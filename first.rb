c7618a1cb848fbfde87e89c74bdc61b287db3286
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2023-05-08 16:42:46 +0900

    四則演算と条件分岐

--- first.rb
+++ first.rb
@@ -2,7 +2,28 @@ puts "Hello"
 puts "ksu"
 # put string
 
-i=5
-i=3
+i=9
 s="ksu"
 puts s+i.to_s
+
+j=2.0
+
+puts i + j
+puts i - j
+puts i * j
+puts i / j
+puts i % j
+
+if i % 2 == 0
+    puts "even"
+else
+    puts "odd"
+end
+
+if i%3==0
+    puts "0"
+elsif i%3==1
+   puts "1"
+else
+   puts "2"
+end
