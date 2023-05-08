1211b4b162415705074722b613db9567bddf2aad
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2023-05-08 17:14:02 +0900

    配列と繰り返し

--- first.rb
+++ first.rb
@@ -34,3 +34,28 @@ while i>0
     i-=1
 end
 puts
+
+ia = [1, 5, 10]
+puts ia[0]
+sa = ["a", "b"]
+puts sa[1]
+xa = [1, "a", 10, [5,3]]
+puts xa[2]
+puts xa[3][0]
+l=0
+while l<ia.length
+    print ia[l]
+    print ","
+    l+=1
+end
+puts
+xa.each do |i|
+    print i
+    print ","
+end
+puts
+
+7.times do 
+    print "*"
+end
+puts
