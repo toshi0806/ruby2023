3add8615779ace645316133272d251f4b693a6fd
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2023-05-29 16:35:54 +0900

    open/close を File.open do に変更

--- fileio.rb
+++ fileio.rb
@@ -1,8 +1,8 @@
-f = File.open "ls.out", "r"
-i=1
-while line = f.gets
-    puts i.to_s + ": " + line
-    i+=1
-end
+File.open("ls.out", "r") do |f|
+    i=1
+    while line = f.gets
+        puts i.to_s + ": " + line
+        i+=1
+    end
 
-f.close
+end
