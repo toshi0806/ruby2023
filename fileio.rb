cbea7d89e5ea22f2b95d2f296f205695998463ba
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2023-05-29 16:55:48 +0900

    変数名を変更

--- fileio.rb
+++ fileio.rb
@@ -1,6 +1,6 @@
-File.open("ls.out", "r") do |f|
+File.open("ls.out", "r") do |file|
     i=1
-    while line = f.gets
+    while line = file.gets
         puts i.to_s + ": " + line
         i+=1
     end
