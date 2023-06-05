81cccbb57fb3b724fcb56f0180fef56d0f24b07d
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2023-06-05 16:00:39 +0900

    while ループで全引数を出力

--- echo.rb
+++ echo.rb
@@ -1,3 +1,15 @@
 # コマンドライン引数の値を画面に表示
-msg=ARGV[0]
-puts msg
+#msg=ARGV[0]
+#puts msg
+
+#for(int i=0; i<ARGV.length; i++) {
+#    print(ARGV[i] + " ");
+#}
+#println();
+
+i=0
+while i<ARGV.length
+    print ARGV[i]+" "
+    i+=1
+end
+puts
