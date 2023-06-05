17d372fa73c18ab51e8557d95d60c726d3d85ff2
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2023-06-05 16:20:36 +0900

    puts を一つにまとめた

--- echo.rb
+++ echo.rb
@@ -1,15 +1,16 @@
 # コマンドライン引数の値を画面に表示
 #msg=ARGV[0]
-#puts msg
-
+#p ARGV
+#String msg="";
 #for(int i=0; i<ARGV.length; i++) {
 #    print(ARGV[i] + " ");
 #}
 #println();
 
+msg=""
 i=0
 while i<ARGV.length
-    print ARGV[i]+" "
+    msg += ARGV[i]+" "
     i+=1
 end
-puts
+puts msg
