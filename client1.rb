be59727de66316bf9c7087d188e55e181331af4c
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2023-06-12 16:44:42 +0900

    HTTP/1.0 で head 情報だけを表示

--- client1.rb
+++ client1.rb
@@ -9,7 +9,10 @@ require "socket"
 port=80
 s=TCPSocket.open "www.is.kyusan-u.ac.jp", port
 s.puts "GET /~toshi/ HTTP/1.0"
+s.puts
 while line=s.gets
+    line.chomp!
+    break if line==""
     puts line
 end
 s.close
