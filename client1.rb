fda06459d33e6f253e7bfe9a7b80cd647c82d6c3
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2023-06-19 17:01:25 +0900

    -h/-b オプションを追加

--- client1.rb
+++ client1.rb
@@ -7,7 +7,22 @@
 # ruby client1.rb
 # ruby client1.rb www.is.kyusan-u.ac.jp /~toshi/index.html
 
+# ruby client1.rb -h www.is.kyusan-u.ac.jp /~toshi/index.html
+# ruby client1.rb -b www.is.kyusan-u.ac.jp /~toshi/index.html
+
 require "socket"
+
+disp_head = false
+disp_body = true
+
+if ARGV[0] == "-h"
+    disp_head = true
+    disp_body = false
+    ARGV.shift
+elsif ARGV[0] == "-b"
+    disp_body = true
+    ARGV.shift
+end
 
 port=80
 host=ARGV[0]
@@ -20,12 +35,13 @@ s.puts
 
 while line=s.gets
     line.chomp!
+    puts line if disp_head
     break if line==""
 end
 
 while line=s.gets
     line.chomp!
-    puts line
+    puts line if disp_body
 end
 
 s.close
