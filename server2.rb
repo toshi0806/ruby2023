700f4ebb4fb34ec6d787e26bea2e6141e504ce24
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2023-07-03 16:48:16 +0900

    path の処理を追加

--- server2.rb
+++ server2.rb
@@ -1,11 +1,31 @@
 require "socket"
 
+# 次回は 簡単な HTML を返してみよう
+# clientX.rb と通信させてみよう
+
 def server s
-    while line=s.gets
-        line.chomp!
-        puts line
-        s.puts line
-        break if line==""
+    cmd, path, ver = s.gets.chomp.split(" ")
+    if path == "/now" 
+        puts Time.now
+        s.puts Time.now
+    elsif path == "/hello"
+        puts "hello, world!"
+        s.puts "HTTP/1.1 200 OK"
+        s.puts "Content-Type: text/html"
+        s.puts
+        s.puts "<DOCTYPE html>"
+        s.puts "<html><body><h1>Hello, World!</h1></body></html>"
+    elsif path == "/"
+        while line=s.gets
+            line.chomp!
+            puts line
+            s.puts line
+            break if line==""
+        end
+    else
+        s.puts "HTTP/1.0 404 Not Found"
+        s.puts
+        s.puts "404 Not Found"
     end
     s.close
 end
